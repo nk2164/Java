33a50b4cbde63f4df6415f6e8f5d14260ca36c38
nk2164 <nk2164@live.com>
2018-05-01 22:34:13 -0500

    minor updates. nothing fancy.

--- src/Deer.java
+++ src/Deer.java
@@ -1,17 +1,14 @@
-// This prints 'DeerReinDeer,false' . Since the reference variable
-// deer points to a ReinDeer in the main method, why is it still
-// printing 'false' ?
-
-public  class Deer {
+class Deer {
   public Deer() {System.out.print("Deer");}
   public Deer(int age) {System.out.print("DeerAge");}
-  private boolean hasHorns() {return false;}
+  private  boolean hasHorns() {return false;}
   public static void main(String[] args) {
-    Deer deer = new ReinDeer(5);
-    System.out.println(","+deer.hasHorns());
+      Deer deer = new ReinDeer(5);
+      System.out.println(","+deer.hasHorns());
   }
+
 }
 class ReinDeer extends Deer {
   public ReinDeer(int age) {System.out.print("ReinDeer");}
-  public boolean hasHorns() {return true;}
+  public  boolean hasHorns() {return true;}
 }
