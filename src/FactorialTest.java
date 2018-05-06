2436872d018826ced901bce65d69550b51dcc68b
nk2164 <nk2164@live.com>
2018-05-06 01:46:50 -0500

    minor updates.

--- src/FactorialTest.java
+++ src/FactorialTest.java
@@ -2,7 +2,7 @@
    This is inspired by the book "Algorithms Unlocked" by Thomas Corman. */
 import java.util.Scanner;
 public class FactorialTest {
-  public static double num = 0;
+  public static long num = 0;
 
   public static void main(String[] args) {
     String inputText;
@@ -19,7 +19,7 @@ public class FactorialTest {
       }
 
       try {
-        num = Double.parseDouble(inputText);
+        num = Long.parseLong(inputText);
         System.out.println("The factorial of the number (n!) is "+ factorial(num));
       }
       catch (NumberFormatException e) {
@@ -29,7 +29,7 @@ public class FactorialTest {
     sc.close();
    }
 
-  private static double factorial(double num){
+  private static long factorial(long num){
     if (num == 0) {
       return 1;
     }
