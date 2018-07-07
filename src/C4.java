05cd452e974c902454d21bf8702956e34ca91bf2
nk2164 <nk2164@live.com>
2018-07-07 04:07:40 -0500

    recent updates.

---
+++ src/C4.java
@@ -0,0 +1,15 @@
+import java.time.format.*;
+import java.time.*;
+import java.util.*;
+// Chapter 4 - Methods and Encapsulation.
+class C4 {
+
+private static final ArrayList<String> values = new ArrayList<>();
+
+public static void main(String[] args) {
+        values.add("changed");
+        ArrayList<String> newvalues = new ArrayList<>();
+        values = newvalues;
+}
+
+}
