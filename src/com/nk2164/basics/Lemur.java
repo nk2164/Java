43aca3771645f70146d82ad5c8faa7858292c2b3
nk2164 <nk2164@live.com>
2018-12-26 00:18:18 -0600

    Added commit message.

--- src/com/nk2164/basics/Lemur.java
+++ src/com/nk2164/basics/Lemur.java
@@ -4,12 +4,10 @@ public class Lemur {
 
 	public static void main(String[] args) {
 		Lemur lemur = new Lemur();
-		Object obj = lemur;
-		
 		lemur.printline();
 	}
 
-	void printline(){
+	void printline() {
 		System.out.println("Hi I am a Lemur");
 	}
 }
