af0eccfcef70429a580e5c98e3224104bdea61f1
nk2164 <nk2164@live.com>
2019-01-12 15:30:44 -0600

    Java - Polymorphism.

---
+++ src/com/nk2164/ocja/chapter5/Bird.java
@@ -0,0 +1,13 @@
+package com.nk2164.ocja.chapter5;
+
+public class Bird {
+	
+	public String  getName() {
+		return "Unknown";
+	}
+	
+	public void displayInformation(){
+		System.out.println("The bird name is: "+getName());
+	}
+
+}
---
+++ src/com/nk2164/ocja/chapter5/Peacock.java
@@ -0,0 +1,17 @@
+package com.nk2164.ocja.chapter5;
+
+import com.nk2164.basics.Bird;
+
+public class Peacock extends Bird {
+
+	@Override
+	public String getName() {
+		return "Peacock";
+	}
+	
+	public static void main(String[] args) {
+		Bird bird = new Peacock();
+		bird.displayInformation();
+	}
+
+}
