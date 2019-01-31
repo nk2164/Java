c60ba2cfe359bf02704fa5f2c2254c4e5e4739f7
nk2164 <nk2164@live.com>
2019-01-30 19:16:48 -0600

    more updates

--- src/com/nk2164/ocja/chapter5/Mammal.java
+++ src/com/nk2164/ocja/chapter5/Mammal.java
@@ -1,11 +1,11 @@
 package com.nk2164.ocja.chapter5;
 
 public class Mammal {
-public Mammal(int age) {
-	// TODO Auto-generated constructor stub
-	super();
-}
-public Mammal() {
-	// TODO Auto-generated constructor stub
-}
+	public Mammal(int age) {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	public Mammal() {
+		// TODO Auto-generated constructor stub
+	}
 }
