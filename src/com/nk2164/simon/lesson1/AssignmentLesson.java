7c659d4781e9a39322188c5c866dc57e714e3276
nk2164 <nk2164@live.com>
2019-03-13 08:00:42 -0500

    Checking in code from assignment lesson

---
+++ src/com/nk2164/simon/lesson1/AssignmentLesson.java
@@ -0,0 +1,23 @@
+package com.nk2164.simon.lesson1;
+
+public class AssignmentLesson {
+	
+	public static void main(String[] args) {
+//		int x = 99;
+//		short y = (short) x;
+		
+//		Integer y = 123; //autoboxing
+//		
+//		String s = y.toString();
+//		
+//		System.out.println("The string s is " + s);
+		
+		String s = "Hello";
+		Class d = String.class;
+		
+		if (s instanceof String) {
+			System.out.println("s is a string!");
+		}
+	}
+
+}
