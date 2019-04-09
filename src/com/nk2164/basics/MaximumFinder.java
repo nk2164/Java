38078440320f61b7aeb8d719b70325977b20f3ab
nk2164 <nk2164@live.com>
2019-04-09 07:56:12 -0500

    Find the maximum of the 3 numbers

--- src/com/nk2164/basics/MaximumFinder.java
+++ src/com/nk2164/basics/MaximumFinder.java
@@ -1,7 +1,30 @@
 package com.nk2164.basics;
 import java.util.Scanner;
 public class MaximumFinder {
+	private static Scanner sc;
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		sc = new Scanner(System.in);
+		System.out.println("Enter 3 floating point numbers saperated by spaces :");
+		
+		double number1 = sc.nextDouble();
+		double number2 = sc.nextDouble();
+		double number3 = sc.nextDouble();
+		
+		System.out.println("The max number is " + findMax(number1,number2,number3));
+	}
+	
+	private static double findMax(double number1,double number2,double number3) {
+		//Set number1 to max to begin with.
+		double maxNumber = number1;
+		
+		if (number2 > maxNumber) {
+			maxNumber = number2;
+		}
+		else if (number3 > maxNumber) {
+			maxNumber = number3;
+		}
+		
+		return maxNumber;
 	}
 }
