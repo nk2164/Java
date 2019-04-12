c3861d2db3920f0c6a18b67f7c6f039686b48431
nk2164 <nk2164@live.com>
2019-04-11 21:14:18 -0500

    Write a simple ternary class

---
+++ src/com/nk2164/basics/TernaryOperator.java
@@ -0,0 +1,10 @@
+package com.nk2164.basics;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		int j = 0;
+		System.out.println((j==0)?"J is Zero !":"J is not Zero !");
+	}
+
+}
