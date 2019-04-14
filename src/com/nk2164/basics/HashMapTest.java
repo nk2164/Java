cc18ff614fe26154ce1052a751e89f9eaa43ccad
nk2164 <nk2164@live.com>
2019-04-14 02:30:58 -0500

    Testing out Hashmaps

---
+++ src/com/nk2164/basics/HashMapTest.java
@@ -0,0 +1,14 @@
+package com.nk2164.basics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest {
+	public static void main(String[] args) {
+		Map<String,Integer> mapOfStringIntPairs = new HashMap<String, Integer>();	
+		
+		String t = "Hi";
+		System.out.println(t);
+		
+	}
+}
