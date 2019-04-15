50929a3fd384dd0f8933ae43a57041a15c73ed0d
nk2164 <nk2164@live.com>
2019-04-14 20:56:43 -0500

    Simple class to demonstrate Hashmap

--- src/com/nk2164/basics/HashMapTest.java
+++ src/com/nk2164/basics/HashMapTest.java
@@ -4,11 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HashMapTest {
+
 	public static void main(String[] args) {
-		Map<String,Integer> mapOfStringIntPairs = new HashMap<String, Integer>();	
+		Integer[] arrayOfIntegers = {22,32,42,52,62,72,82,92};
+		String[]  arrayOfString   = {"A","B","C","D","E","F","G","H"};
+	
+		Map<String,Integer> mapOfStringIntPairs = new HashMap<String, Integer>();
 		
-		String t = "Hi";
-		System.out.println(t);
+		for (int j = 0; j < arrayOfIntegers.length; j++) {
+			mapOfStringIntPairs.put(arrayOfString[j], arrayOfIntegers[j]);
+		}
+		
+		String key = "X";
+		
+		if (mapOfStringIntPairs.containsKey(key)) {
+			System.out.printf("Key = %s is present with Value =  %d", key ,mapOfStringIntPairs.get(key));
+		}
+		else {
+			System.out.printf("Key %s is not present.", key);
+		}
 		
 	}
 }
