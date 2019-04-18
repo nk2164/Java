2f69ae7f2b762aa6f90317c8da9aebbb0c3150da
nk2164 <nk2164@live.com>
2019-04-18 00:13:53 -0500

    demonstrate pass by value concepts for primitives and reference var

--- src/com/nk2164/basics/PassArray.java
+++ src/com/nk2164/basics/PassArray.java
@@ -13,11 +13,33 @@ public class PassArray {
 		}
 		
 		modifyArray(array);
-		System.out.printf("The value of the modified array is ", args);
+		
+		System.out.println();
+		
+		System.out.printf("The value of the modified array is %n", args);
 		
 		for (int value:array) {
 			System.out.printf("  %d",value);
 		}
+
+		System.out.println();
+		
+		modifyVar(array[3]);
+		
+		System.out.printf("The value of array[3] after call into modify var proc is %d %n", array[3]);
+		
 	}
+	
+	public static void modifyArray(int[] array) {
+		for (int i=0;i < array.length;i++) {
+			array[i] *= 2;
+		}
+	}
+	
+	public static void modifyVar(int var) {
+			var *= 2;
+			System.out.printf("%nThe value of var inside method is %d %n", var);
+	}
+	
 
 }
