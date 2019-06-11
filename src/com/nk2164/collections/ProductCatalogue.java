28c24d7e4c01763ee9cea38eb68ea0d86f055495
nk2164 <nk2164@live.com>
2019-06-10 22:02:27 -0500

    learning Hash Sets in java

--- src/com/nk2164/collections/ProductCatalogue.java
+++ src/com/nk2164/collections/ProductCatalogue.java
@@ -1,17 +1,22 @@
 package com.nk2164.collections;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class ProductCatalogue implements Iterable<Product> {
 	
+	private final Set<Product> products = new HashSet<>();
+	
 	public void isSuppliedBy(Supplier supplier) {
-		
+		products.addAll(Supplier.products());
 	}
 	
 	@Override
 	public Iterator<Product> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return products.iterator();
 	}
 
 }
--- src/com/nk2164/collections/Supplier.java
+++ src/com/nk2164/collections/Supplier.java
@@ -1,11 +1,22 @@
 package com.nk2164.collections;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Supplier {
 	private String name;
-	
+	private final static Set<Product> products = new HashSet<>();
 	
 	public Supplier(String name) {
 		this.name = name;
+	}
+
+
+	public static Set<Product> products() {
+		// TODO Auto-generated method stub
+		return products;
 	}
 
 }
